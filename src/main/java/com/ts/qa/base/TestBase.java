0ce4d809a5cc54339a654c0455d47ffe048be739
anita0701 <anita.dewangan@townscript.com>
2019-09-10 12:28:17 +0530

    chromeoptions added

--- src/main/java/com/ts/qa/base/TestBase.java
+++ src/main/java/com/ts/qa/base/TestBase.java
@@ -59,7 +59,14 @@ public class TestBase {
 //            chromeOptions.addArguments("--headless");
 //            System.out.println("Initialize chromedriver");
 //            driver = new ChromeDriver(chromeOptions);
-			driver = new ChromeDriver();
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("start-maximized"); // open Browser in maximized mode
+			options.addArguments("disable-infobars"); // disabling infobars
+			options.addArguments("--disable-extensions"); // disabling extensions
+			options.addArguments("--disable-gpu"); // applicable to windows os only
+			options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+			options.addArguments("--no-sandbox"); // Bypass OS security model
+			WebDriver driver = new ChromeDriver(options);
             e_driver = new EventFiringWebDriver(driver);
             event_listener = new WebEventListener();
             e_driver.register(event_listener);
